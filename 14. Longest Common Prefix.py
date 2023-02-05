f07f9224764201cfb7f0dfe1ea2a6f6cc0d9b7b8
Namwoongchan <75806377+woongchan789@users.noreply.github.com>
2023-02-05 18:45:56 +0900

    Create 14. Longest Common Prefix.py

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        n = 0
+        min_len = 200
+        correct_num = 0
+        
+        for i in range(len(strs)):
+            if len(strs[i]) <= min_len:
+                min_len = len(strs[i])
+        
+        while n < min_len:
+            for j in range(len(strs)-1):
+                correct_num += int(strs[j][n] == strs[j+1][n])
+            if correct_num == (len(strs)-1):
+                n += 1
+                correct_num = 0
+            else:
+                break
+
+        return strs[0][:n]
