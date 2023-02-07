29aa196dd2ae37655e31e3cb3bdb2a7c5e5f4e19
Namwoongchan <75806377+woongchan789@users.noreply.github.com>
2023-02-07 17:28:57 +0900

    Delete 1672. Richest Customer Wealth.py

--- 1672. Richest Customer Wealth.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def maximumWealth(self, accounts):
-        richest = 0
-        for i in range(len(accounts)):
-            if richest < sum(accounts[i]):
-                richest = sum(accounts[i])
-        return richest
