3810e19f2805f77c51eb13d0a7e6a2c6699d7da4
Namwoongchan <75806377+woongchan789@users.noreply.github.com>
2023-02-07 17:28:05 +0900

    Delete 1. Two Sum.py

--- 1. Two Sum.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                if (nums[i] + nums[j]) == target and (i != j):
-                    return i,j
