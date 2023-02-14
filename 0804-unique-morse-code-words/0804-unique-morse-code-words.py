4a5ea30b25e559261c71cd7c11acc09f18698ae0
Namwoongchan <75806377+woongchan789@users.noreply.github.com>
2023-02-14 21:58:03 +0900

    Time: 11 ms (99.68%), Space: 13.7 MB (19.24%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        morse_list = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.", \
+                      "--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        
+        cnt_dict = {}
+        
+        for i in words:
+            temp = ''
+            for w in i:
+                temp += morse_list[ord(w) - 97]
+            if temp not in cnt_dict:
+                cnt_dict[temp] = 1
+            else:
+                cnt_dict[temp] += 1
+        
+        return len(cnt_dict)
+        
