541ae689b273e8e9b561a9cc69af217e77d8cc69
Namwoongchan <75806377+woongchan789@users.noreply.github.com>
2023-04-20 18:29:05 +0900

    Time: 22 ms (86.26%), Space: 13.8 MB (61.61%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        i=j=1
+        n = len(s)
+        opening_count = 1
+        closing_count = 0
+        result = ""
+        while j<n: 
+            if  opening_count != closing_count:
+                if s[j] == "(":
+                    opening_count +=1
+                else:
+                    closing_count +=1
+                j +=1
+                if j ==n and  opening_count == closing_count :
+                    result += s[i:j-1]
+            else:
+                result += s[i:j-1]
+                j += 1
+                i = j
+                opening_count = 1
+                closing_count = 0
+        return result
